c009d48e631f265d83a211dacdddf6a567a24a39
Yevhen Onoprienko <fromzpcity@gmail.com>
2025-11-14 15:34:50 +0200

    03. usefull-things-046-navigation. Bg.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,13 @@
+
 var tl = new TimelineMax({ paused: true });
 
 tl.to(".menu-container", 0.4, {
     right: "0%"
 });
 
-tl.staggerFrom(".link", 0.4, { opacity: 0 }, 0.1, "-=0.2");
+tl.staggerFrom(".link", 0.5, {
+    opacity: 0
+}, 0.1, "-=0.2");
 
 tl.reverse();
 
@@ -15,23 +18,6 @@ $(document).on("click", ".open-menu", function () {
 $(document).on("click", ".close-menu", function () {
     tl.reversed(!tl.reversed());
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
